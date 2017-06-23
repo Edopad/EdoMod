9c1f44a4bc41aedc6af9fdc46670eaeedda41ddf
mgrant8164 <mgrant8164@gmail.com>
2017-06-22 21:51:01 -0700

    funkashed

---
+++ build/src/turban/Turban.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DuckGame.EdoMod
+{
+    class Turban : EdoHat
+    {
+
+
+        public Turban(float x, float y, Team t) : base(x, y, t)
+        {
+        }
+    }
+}
